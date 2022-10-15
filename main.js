28b6b297a63f69e113578fb641a3c27f085c95d8
Lathadarsh <97331941+Lathadarsh@users.noreply.github.com>
2022-10-15 17:37:32 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
+noseX=0;
+noseY=0;
+
 function preload() {
+    mustache = loadImage("https://i.postimg.cc/g0mCMKvH/bigotes-black-mustache-png-clipart-removebg-preview.png");
 }
 
 function setup() {
@@ -16,13 +20,16 @@ function modelLoaded() {
 function gotPoses(results) {
     if(results.length > 0) {
         console.log(results);
-        console.log("nose x = " + results[0].pose.nose.x);
-        console.log("nose y = " + results[0].pose.nose.y);
+        noseX= results[0].pose.nose.x;
+        noseY= results[0].pose.nose.y;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
     }
 }
 
 function draw() {
     image(video,0,0,300,300);
+    image(mustache, noseX-33, noseY-5, 70,50);
 }
 
 function take_snapshot() {
